088b6eaa5f5f234d64c9499e2aff2b54dad872e0
Ihor Pankiv <ihorpankiv.dev@gmail.com>
2024-07-25 17:59:20 +0300

    Added pagination on the log file page

--- LogSearcherWeb/Controllers/LogSearchController.cs
+++ LogSearcherWeb/Controllers/LogSearchController.cs
@@ -27,7 +27,7 @@ namespace LogSearcherWeb.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> ViewLogFile(string highlightLine, string filePath, int lineNumber = 0)
+        public async Task<IActionResult> ViewLogFile(string highlightLine, string filePath, int? pageNumber, int pageSize = 10)
         {
             var logLines = new List<string>();
 
@@ -40,12 +40,28 @@ namespace LogSearcherWeb.Controllers
                 }
             }
 
+            int highlightLineIndex = logLines.FindIndex(line => line.Contains(highlightLine));
+            if (highlightLineIndex != -1 && !pageNumber.HasValue)
+            {
+                pageNumber = (highlightLineIndex / pageSize) + 1;
+            }
+
+            pageNumber = pageNumber ?? 1;
+
+            var paginatedLines = logLines.Skip((pageNumber.Value - 1) * pageSize).Take(pageSize).ToList();
+            var totalLines = logLines.Count;
+
             var model = new LogFileViewModel
             {
                 FilePath = filePath,
-                Lines = logLines,
-                HighlightLine = highlightLine
+                Lines = paginatedLines,
+                PageNumber = pageNumber.Value,
+                PageSize = pageSize,
+                TotalLines = totalLines,
+                HighlightLine = highlightLine,
+                HighlightLineIndex = highlightLineIndex
             };
+
             return View(model);
         }
     }
--- LogSearcherWeb/Models/LogFileViewModel.cs
+++ LogSearcherWeb/Models/LogFileViewModel.cs
@@ -5,6 +5,10 @@
         public string FilePath { get; set; }
         public List<string> Lines { get; set; } = new List<string>();
         public string HighlightLine { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalLines { get; set; }
+        public int HighlightLineIndex { get; set; }
     }
 
 }
